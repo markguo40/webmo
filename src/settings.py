bebd89e3e204754397921780a7e6fef4ba631670
Zhanchen Guo <markguo40@gmail.com>
2015-12-17 15:57:06 -0800

    debug

--- src/settings.py
+++ src/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(zvzzqyvq^@(%f%p+tao8s_0-jgp_)mvdz!ss4m$h_j!=oy$wj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webmo.herokuapp.com']
 
 
 # Application definition
